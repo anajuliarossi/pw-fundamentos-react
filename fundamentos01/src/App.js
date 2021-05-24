122038e9d88487645334a7d85d38d1862eb55e3c
anajuliarossi <rossianajulia1@gmail.com>
2021-05-24 18:10:39 +0000

    Aula01 - Commit inicial
    Signed-off-by: anajuliarossi <rossianajulia1@gmail.com>

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,16 @@
+import Reac, {Component} from 'react'
+import OlaMundo from './componentes/basico/OlaMundo'
+class App extends Component{
+  render(){
+    return( 
+      <div> 
+        <h1>Aula 01 - Fundamentos 01</h1>
+
+        <OlaMundo/>
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ fundamentos01/src/componentes/basico/OlaMundo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default () => {
+  const informacao ='Primeiro componente JSX'
+  return ( 
+ <div> 
+  <h2>Componente OlaMundo</h2>
+  <p>{ informacao }</p>
+ </div>
+  )
+  }
